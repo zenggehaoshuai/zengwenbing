5b9329c31b6a01248c4dc6975a9f632dd5a30abb
zenggehaoshuai <461153861@qq.com>
2013-08-23 00:30:13 +0800

    delete

--- microblog/controllers/someone/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: zengwenbing
- * Date: 13-8-13
- * Time: 下午6:40
- * To change this template use File | Settings | File Templates.
- */
--- microblog/model/mongoose.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-module.exports = {
-     mongoose:mongoose,
-     db : mongoose.createConnection('mongodb://127.0.0.1:27017/microblog')
-}
